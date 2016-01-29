e72a6da3b287bc18ac1490f10b13f88d3374048d
jmanock <jonmanock@gmail.com>
2016-01-29 07:30:16 -0500

    new example

---
+++ javascripts/chirpApp.js
@@ -0,0 +1,39 @@
+var app = angular.module('chirpApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
+app.controller('mainController', function($scope){
+  $scope.posts = [];
+  $scope.newPost = {created_by:'', text:'', created_at:''};
+
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by:'', text:'', created_at:''};
+  };
+
+  $scope.login = function(){
+    $scope.error_message = 'login request for '+ $scope.user.username;
+  };
+
+  $scope.register = function(){
+    $scope.error_message = 'registeration request for '+ $scope.user.username;
+  };
+
+});
